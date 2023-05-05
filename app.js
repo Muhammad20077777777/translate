5978dedd77e8c9650e7b78846eedd8120e48f079
mmm <127667859+Muhammad20077777777@users.noreply.github.com>
2023-05-05 17:29:29 +0500

    aaa

---
+++ app.js
@@ -0,0 +1,159 @@
+
+let fruits = prompt("Xohlagan mevangizni kiriting") 
+ 
+switch (fruits) { 
+    case "olma": 
+        alert("apple") 
+        break; 
+    case "banan": 
+        alert("banana") 
+        break; 
+    case "ananas": 
+        alert("pineapple") 
+        break; 
+ 
+    case "qulupnay": 
+        alert("strawberry") 
+        break; 
+    case "nok": 
+        alert("pear") 
+        break; 
+    case "uzum": 
+        alert("grapes") 
+        break; 
+ 
+    case "mandarin": 
+        alert("tangerine") 
+        break; 
+    case "apelsin": 
+        alert("orange") 
+        break; 
+    case "anor": 
+        alert("pomegranate") 
+        break; 
+ 
+    case "anjir": 
+        alert("fig") 
+        break; 
+ 
+    case "shaftoli": 
+        alert("peach") 
+        break; 
+ 
+    case "malina": 
+        alert("paspberry") 
+        break; 
+ 
+    case "gilos": 
+        alert("cherry") 
+        break; 
+    case "mango": 
+        alert("mango") 
+        break; 
+    case "kivi": 
+        alert("kiwi") 
+        break; 
+ 
+    case "kakos": 
+        alert("coconut") 
+        break; 
+    case "aprel": 
+        alert("Bahor") 
+        break; 
+    case "olxo'ri": 
+        alert("plum") 
+        break; 
+ 
+    case "xurmo": 
+        alert("Persimon") 
+        break; 
+    
+    case "limon": 
+        alert("limon") 
+        break; 
+    case "mango": 
+        alert("mangoes") 
+        break; 
+ 
+    case "feyxoa": 
+        alert("feijoa") 
+        break; 
+ 
+    case "tarvuz": 
+        alert("watermelon") 
+        break; 
+ 
+    case "qovun": 
+        alert("melon") 
+        break; 
+ 
+    case "kartoshka": 
+        alert("potato") 
+        break; 
+ 
+    case "pomidor": 
+        alert("tomato") 
+        break; 
+ 
+    case "sabzi": 
+        alert("carrot") 
+        break; 
+ 
+    case "lavlagi": 
+        alert("beetroot") 
+        break; 
+ 
+ 
+    case "karam": 
+        alert("cabbage") 
+        break; 
+ 
+    case "turp": 
+        alert("horseradish") 
+        break; 
+ 
+ 
+    case "rediska": 
+        alert("radish") 
+        break; 
+ 
+    case "baqlajon": 
+        alert("eggplant") 
+        break; 
+ 
+ 
+    case "qalampir": 
+        alert("papper") 
+        break; 
+ 
+    case "piyoz": 
+        alert("onion") 
+        break; 
+ 
+    case "sarimsoq": 
+        alert("garlic") 
+        break; 
+ 
+    case "petrushka": 
+        alert("parsley") 
+        break; 
+ 
+    case "sholg'om": 
+        alert("turnip") 
+        break; 
+ 
+    case "bodring": 
+        alert("cucumber") 
+        break; 
+ 
+    case "no'xat": 
+        alert("peas") 
+        break; 
+ 
+    case "loviya": 
+        alert("bean") 
+        break; 
+ 
+    default: 
+        alert("Bu noto'g'ri so'z") 
+}
